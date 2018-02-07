ef109d20e144201cb8d6efd97b85776cc0f7bf2d
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-02-07 16:18:06 +0530

    Create 7.2.numeric digits.c

---
+++ 7.2.numeric digits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int n,count=0;
+printf("\n enter the num:");
+scanf("%d",&n);
+ while(n!=0)
+  {
+       n/=10;
+       count++;
+    }
+printf("\n the no of digits is :%d",count);
+ return 0;
+}
