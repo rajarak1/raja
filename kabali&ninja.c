94e5d10020c95669a8a088633c4307786b9f42b7
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-02-23 16:10:59 +0530

    Create kabali&ninja.c

---
+++ kabali&ninja.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int kabali_ninja,opppnent_ninja,i,t;
+for(i=0;i<3;i++)
+{
+scanf("%d %d",&kabali_ninja,&opppnent_ninja);
+printf("\n %d",opppnent_ninja-kabali_ninja);
+}
+return 0;
+}
