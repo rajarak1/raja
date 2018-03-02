9fc6c8df7a74c405ae739d9e3a623e3e55bca35a
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-02 15:43:13 +0530

    Create string s.c

---
+++ string s.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int n,l,x,i;
+	char a[100];
+	gets(a);
+	scanf("%d",&n);
+	l=strlen(a);
+	x=l-n;
+	for(i=x;i<l;i++)
+	{
+		printf("%c",a[i]);
+	}
+	return(0);	
+}
