044678add65e842ad4fecc95421479c1da60ef3b
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-05 15:53:09 +0530

    Create number of repetition.c

---
+++ number of repetition.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,k,a[100],i,count=0;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==k)
+		{
+			count++;
+		}
+	}
+	printf("%d\n  occurs %d \n times",k,count);	
+}
