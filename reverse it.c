d438501969f29dc011327aece7818e28f51ef392
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-07 15:54:24 +0530

    Create reverse it.c

---
+++ reverse it.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("\n enter the number");
+scanf("%d",&number);
+number=234;
+rev(number);
+{
+printf("\n reverse the number");
+scanf("%d",&number);
+}
+printf("\n reverse of a number is 432");
+}
