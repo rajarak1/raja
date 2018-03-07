7bbd3daf123b743bca93b7c6e57c19348ac74610
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-07 15:52:33 +0530

    Update factorial a.c

--- factorial a.c
+++ factorial a.c
@@ -1,8 +1,8 @@
 n=int(input())
 if n<0:
-	print("positive numbers only")
+	print("\n positive numbers only")
 elif n==0:
-	print("factorial of zero is 1")
+	print("\nfactorial of zero is 1")
 else:
 	fact=1
 	for i in range(1,n+1):
