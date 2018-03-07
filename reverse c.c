38e728389035bf79d18dc6eff45162170fa81877
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-07 15:44:35 +0530

    Create reverse c.c

---
+++ reverse c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    gets(str);
+    int i, len;
+    len=strlen(str);
+    for(i=len;i>=0;i--)
+     printf("%c",str[i]);
+    return(0);
+}
