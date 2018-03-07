44286e53b72ffda9b388d703571e0f2bf4a32ed2
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-07 15:50:28 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,fact=1;
+    cin>>n;
+    for(i=n;i>0;i--)
+    {
+      fact=fact*i;  
+    }
+    cout<<\n fact;
+    return 0;
+}
