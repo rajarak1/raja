6d9b2e37c81e5d9e2f681dd303ee144458ed92a8
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-09 15:50:09 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char a[10];
+	scanf("%s",a);
+	if(a[0]=='S'||a[0]=='s')
+	{
+	    printf("yes its a holiday");
+	}
+	else
+	{
+	 printf("its not a holiday");   
+	}
+	return 0;
+}
