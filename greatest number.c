d85f936db4cdc7e57387c7b196b86a2672078ed5
rajarak1 <35295960+rajarak1@users.noreply.github.com>
2018-03-14 15:41:52 +0530

    Create greatest number.c

---
+++ greatest number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main(void) 
+{
+	int n,k,i,len;
+	scanf("%d %d",&n,&k);
+	if(n>k)
+	{
+		len=n;
+	}
+	else
+	{
+		len=k;
+	}
+	for(i=len;i>=2;i--)
+	{
+		if(n%i==0 && k%i==0)
+		{
+			printf("%d\n ",i);		
+		}
+	}
+
+	return 0;
+}
